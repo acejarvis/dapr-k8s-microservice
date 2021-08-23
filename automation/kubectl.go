9c242b9a7a8e69fe28974ac481a4596d1fdf11a9
acejarvis <acejarvis98@gmail.com>
2021-08-23 21:05:40 +0000

    add request

--- automation/kubectl.go
+++ automation/kubectl.go
@@ -89,14 +89,17 @@ func (k *KubeClient) CreateAppDeploy() string {
 	return fmt.Sprintf("Created deployment %q.\n", result.GetObjectMeta().GetName())
 }
 
-func (k *KubeClient) DeleteAppDeploy() string {
-	deploymentsClient := k.Clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
+func (k *KubeClient) DeleteAppDeploy(namespace string, appName string) string {
+	if namespace == "" {
+		namespace = "default"
+	}
+	deploymentsClient := k.Clientset.AppsV1().Deployments(namespace)
 	fmt.Println("Deleting deployment...")
 	deletePolicy := metav1.DeletePropagationForeground
-	if err := deploymentsClient.Delete(context.TODO(), "demo-deployment", metav1.DeleteOptions{
+	if err := deploymentsClient.Delete(context.TODO(), appName, metav1.DeleteOptions{
 		PropagationPolicy: &deletePolicy,
 	}); err != nil {
-		panic(err)
+		return fmt.Sprintf("error: %v", err)
 	}
 	fmt.Println("Deleted deployment.")
 	return fmt.Sprintf("Deleted deployment.")
@@ -104,15 +107,64 @@ func (k *KubeClient) DeleteAppDeploy() string {
 
 func (k *KubeClient) ConnectRedis() string {
 	deploymentsClient := k.Clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
-	fmt.Println("Deleting deployment...")
-	deletePolicy := metav1.DeletePropagationForeground
-	if err := deploymentsClient.Delete(context.TODO(), "demo-deployment", metav1.DeleteOptions{
-		PropagationPolicy: &deletePolicy,
-	}); err != nil {
+	fmt.Println("Connecting Redis...")
+
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "demo-deployment",
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: int32Ptr(2),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "demo",
+				},
+			},
+			Template: apiv1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": "demo",
+					},
+				},
+				Spec: apiv1.PodSpec{
+					Containers: []apiv1.Container{
+						{
+							Name:  "web",
+							Image: "nginx:1.12",
+							Ports: []apiv1.ContainerPort{
+								{
+									Name:          "http",
+									Protocol:      apiv1.ProtocolTCP,
+									ContainerPort: 80,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	result, err := deploymentsClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
+	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Deleted deployment.")
-	return fmt.Sprintf("Deleted deployment.")
+	fmt.Printf("Created deployment %q.\n", result.GetObjectMeta().GetName())
+	return fmt.Sprintf("Created deployment %q.\n", result.GetObjectMeta().GetName())
+}
+
+func (k *KubeClient) DisconnectDCS(namespace string, appName string) string {
+	deploymentsClient := k.Clientset.CoreV1().ComponentStatuses()
+	fmt.Println("Deleting DCS...")
+	// deletePolicy := metav1.DeletePropagationForeground
+	if err := deploymentsClient.Delete(context.TODO(), "statestore", metav1.DeleteOptions{
+		// PropagationPolicy: &deletePolicy,
+	}); err != nil {
+		fmt.Println("error: ", err)
+		return fmt.Sprintf("error: %v", err)
+	}
+	fmt.Println("Deleted DCS.")
+	return fmt.Sprintf("Deleted DCS.")
 }
 
 func int32Ptr(i int32) *int32 { return &i }
